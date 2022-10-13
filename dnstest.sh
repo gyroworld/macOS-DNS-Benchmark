34e948eb6488018a8ca9720ee83f567a2af31ce1
gyroworld <dieselg5@gmail.com>
2022-10-13 17:32:25 -0400

    Update dnstest.sh

--- dnstest.sh
+++ dnstest.sh
@@ -1,12 +1,13 @@
 #macOS DNS Benchmark
-
+IFS=$' '
 BENCHMARK_DNS_IPS=("1.1.1.1" "8.8.8.8")
 BENCHMARK_DNS_TEXT=("Cloudflare DNS" "Google DNS")
 SYSTEM_DNS_SERVERS=($(grep nameserver <(scutil --dns) | awk '{print $3}' | uniq))
 DEFAULT_GATEWAY=$(route get default | grep gateway | awk '{print $2}')
 DEFAULT_GATEWAY_IN_SYSTEM_DNS=false
 DEFAULT_GATEWAY_IS_DNS=false
-DOMAIN_COUNT=$(wc -l domains2.list | awk '{print $1}')
+DOMAINS=$(curl -s https://raw.githubusercontent.com/gyroworld/macOS-DNS-Benchmark/main/domains.list)
+DOMAIN_COUNT=$(echo $DOMAINS | wc -l | awk '{print $1}')
 
 function get_dns_servers() {
     #Check if default gateway in part of system DNS servers
@@ -21,7 +22,7 @@ function get_dns_servers() {
     #If it's not, check if it has a DNS server running
     if [ $DEFAULT_GATEWAY_IN_SYSTEM_DNS = false ]; then
         check=$(nslookup -retry=1 -timeout=1 example.com $DEFAULT_GATEWAY)
-        if [[ $check == *"connection timed out"* ]]; then
+        if [[ "${check}" == *"connection timed out"* ]]; then
             echo "Default gateway not a DNS server."
         else
             DEFAULT_GATEWAY_IS_DNS=true
@@ -33,7 +34,7 @@ function query_dns_server() {
     dscacheutil -flushcache
     printf "\n"
     printf "\e[1;32mDNS Server: $1 ($2)\e[0m"
-    time dig +tries=1 +time=2 @$1 -f domains.list +noall +answer >/dev/null
+    time dig +tries=1 +time=2 @$1 -f <(echo $DOMAINS) +noall +answer >/dev/null
 }
 
 function run_benchmark() {
